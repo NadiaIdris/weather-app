15e33734d22bca9c874a31a3a981ab314fd32a3b
Nazmul Idris <nazmul@fasterlap.com>
2019-09-29 19:32:44 -0700

    Add constants for keys in LocalStorage
    Best practices for JS constant naming:
    https://stackoverflow.com/a/51884773/2085356

--- src/storage.js
+++ src/storage.js
@@ -1,17 +1,23 @@
+/**
+ * The following keys are stored in LocalStorage.
+*/
+const LOCATIONS = {
+  WEATHER_DATA: "WEATHER_DATA",
+  LAT: "LAT",
+  LNG: "LNG"
+};
+
 const save = (key, weatherData) => {
   localStorage.setItem(key, JSON.stringify(weatherData));
-}
+};
 
 const load = (key) => {
   const dataFromStorage = JSON.parse(localStorage.getItem(key));
-  if (!dataFromStorage)  return;
+  if (!dataFromStorage) {
+    return;
+  }
   console.log(`This is data from localStorage`);
   console.log(dataFromStorage);
-}
+};
 
-// const saveAndLoadData = (key, weatherData) => {
-//   save('weatherData', weatherData);
-//   load('weatherData');
-// }
-
-export {save, load};
+export {save, load, LOCATIONS};
