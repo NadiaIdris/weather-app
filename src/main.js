6a0426c0eb816cc644a6b040da45f86e460b9021
Maret Idris <maret@fasterlap.com>
2019-11-13 12:49:00 -0800

    Move link to LinkedIn icon to the right of the search bar

--- src/main.js
+++ src/main.js
@@ -34,11 +34,6 @@ const initializePlacesApi = () => {
   });
 };
 
-window.addEventListener('resize', () => {
-  const vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
-
 const main = () => {
   initializePlacesApi();
   attachListeners();
